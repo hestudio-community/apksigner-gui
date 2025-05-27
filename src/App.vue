c8db5e5050aa30553fcbcc474ec5d117c3eed358
undefined <zihengdie@hestudio.net>
2025-05-28 00:17:23 +0800

    fix(UI): Optimize el-input-number button rounding.

--- src/App.vue
+++ src/App.vue
@@ -256,13 +256,13 @@
 .el-input-number__decrease {
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
-  margin-top: 3%;
+  margin-top: 5.5px
 }
 
 .el-input-number__increase {
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
-  margin-top: 3%;
+  margin-top: 5.5px
 }
 
 .el-button {
